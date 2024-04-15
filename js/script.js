cdc4e881f5e6cee5c8abbe250d53ee86a103083b
ascm00 <martin.aschermann2@gmail.com>
2024-04-15 23:13:27 +0200

    hamburger menu edit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.getElementById('menu-btn');
-    const menu = document.getElementById('menu');
+    const menu = document.querySelector('nav');
 
     menuBtn.addEventListener('click', function() {
-        // Toggle visibility
-        if (menu.classList.contains('hidden')) {
-            menu.classList.remove('hidden');
-        } else {
-            menu.classList.add('hidden');
-        }
+        menu.classList.toggle('hidden');
     });
 });
